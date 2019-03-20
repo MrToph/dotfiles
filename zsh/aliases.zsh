111f4f245d4e0c2a919c5a334ecd901923a3510b
MrToph <christoph.michel.1992@gmail.com>
2019-03-20 14:25:49 +0100

    sort gitbranches ascending

--- zsh/aliases.zsh
+++ zsh/aliases.zsh
@@ -1,4 +1,4 @@
 alias reload!='. ~/.zshrc'
 
 alias cls='clear' # Good 'ol Clear Screen command
-alias gitbranches='git branch --sort=committerdate'
+alias gitbranches='git branch --sort=-committerdate'
